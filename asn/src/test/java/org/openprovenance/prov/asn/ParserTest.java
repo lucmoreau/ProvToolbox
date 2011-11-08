89c2c231aefcc75c2d595a5435ad3f4fafc23a6d
Luc Moreau <luc.moreau@uprovenance.com>
2011-11-08 22:28:46 +0000

    asn

---
+++ asn/src/test/java/org/openprovenance/prov/asn/ParserTest.java
@@ -0,0 +1,32 @@
+package org.openprovenance.prov.asn;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class ParserTest extends TestCase
+{
+    public void testContainer0() {
+        testContainer("src/test/resources/prov/container0.prov");
+    }
+
+    public void testContainer1() {
+        testContainer("src/test/resources/prov/container1.prov");
+    }
+
+    public void testContainer2() {
+        testContainer("src/test/resources/prov/container2.prov");
+    }
+
+    public void testContainer(String file) {
+        try {
+            new MainParser().getParserForFile(file).container();
+        } catch (Throwable t) {
+            System.out.println("exception: "+t);
+            t.printStackTrace();
+       
+            assertTrue(false);
+        }
+    }
+}
+
+

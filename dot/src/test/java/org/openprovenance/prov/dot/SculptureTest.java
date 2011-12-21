bfb10f8fd503fbc246b55970e5303920445d3b2c
Luc Moreau <l.moreau@ecs.soton.ac.uk>
2011-12-21 10:00:01 +0000

    added sculpture example

---
+++ dot/src/test/java/org/openprovenance/prov/dot/SculptureTest.java
@@ -0,0 +1,41 @@
+package org.openprovenance.prov.dot;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+import org.openprovenance.prov.xml.ProvFactory;
+import org.openprovenance.prov.xml.ProvSerialiser;
+import org.openprovenance.prov.xml.Container;
+
+import javax.xml.bind.JAXBException;
+import java.io.File;
+
+import org.antlr.runtime.tree.CommonTree;
+import org.openprovenance.prov.asn.Utility;
+
+public class SculptureTest extends TestCase {
+
+    public void sculptureToDot(String asnFile, String xmlFile, String dotFile, String pdfFile)
+        throws java.io.FileNotFoundException,  java.io.IOException, JAXBException, Throwable {
+        Utility u=new Utility();
+
+        CommonTree tree = u.parseASNTree(asnFile);
+
+        Container o= (Container) u.convertToJavaBean(tree);
+
+        ProvSerialiser serial=ProvSerialiser.getThreadProvSerialiser();
+        
+        serial.serialiseContainer(new File(xmlFile),o,true);
+
+        ProvToDot toDot=new ProvToDot("src/main/resources/defaultConfigWithRoleNoLabel.xml"); 
+        
+        toDot.convert(o,dotFile,pdfFile);
+    }
+
+    public void testSculptureToDot1() throws java.io.FileNotFoundException,  java.io.IOException, JAXBException, Throwable {
+        sculptureToDot("../asn/src/test/resources/prov/sculpture.prov-asn",
+                       "target/sculpture.prov-xml",
+                       "target/sculpture.dot",
+                       "target/sculpture.pdf");
+    }
+}

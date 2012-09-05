c2597893698b512aaa9a18bcbb817598c69a0b97
Luc Moreau <luc.moreau@uprovenance.com>
2012-09-06 00:25:38 +0100

    eclipse configuration

--- prov-xml/src/main/java/org/openprovenance/prov/xml/Element.java
+++ prov-xml/src/main/java/org/openprovenance/prov/xml/Element.java
@@ -1,6 +1,4 @@
 package org.openprovenance.prov.xml;
 
-import java.util.List;
-
 public interface Element extends HasType, HasLabel, Identifiable, HasExtensibility {
 } 
--- prov-xml/src/main/java/org/openprovenance/prov/xml/HasExtensibility.java
+++ prov-xml/src/main/java/org/openprovenance/prov/xml/HasExtensibility.java
@@ -2,6 +2,7 @@ package org.openprovenance.prov.xml;
 import java.util.List;
 
 public interface HasExtensibility {
-        public List getAny();
-    public java.util.Hashtable<String,List<Object>> attributesWithNamespace(String namespace);
+	@SuppressWarnings("rawtypes")
+	public List getAny();
+	public java.util.Hashtable<String,List<Object>> attributesWithNamespace(String namespace);
 } 

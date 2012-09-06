2f6fca90f7cb893fc372bf292a3068941d34c2b0
Luc Moreau <luc.moreau@uprovenance.com>
2012-09-06 20:24:22 +0100

    updated tests

--- prov-dot/src/test/java/org/openprovenance/prov/dot/PC1FullTest.java
+++ prov-dot/src/test/java/org/openprovenance/prov/dot/PC1FullTest.java
@@ -18,23 +18,22 @@ public class PC1FullTest extends org.openprovenance.prov.xml.PC1FullTest {
     }
 
     @Override
-    public void subtestPC1Full() throws JAXBException {
-    }
-
-    @Override
     public void subtestCopyPC1Full() throws java.io.FileNotFoundException,  java.io.IOException   {
     }
 
+    @Override
     public void testPC1() throws JAXBException, java.io.FileNotFoundException,  java.io.IOException, SAXException   {
         super.testPC1();
-        subtestToDot();
+        subtestDoToDot();
     }
     
-    public void subtestToDot() throws FileNotFoundException, IOException {
+    void subtestDoToDot() throws FileNotFoundException, IOException {
 	/** Produces a dot representation
 	 * of created graph. */
 
         ProvToDot toDot=new ProvToDot(true); // with roles
+
+	if (graph1==null) System.out.println("doToDot with null ");
         
         toDot.convert(graph1,"target/pc1-full.dot", "target/pc1-full.pdf");
     }

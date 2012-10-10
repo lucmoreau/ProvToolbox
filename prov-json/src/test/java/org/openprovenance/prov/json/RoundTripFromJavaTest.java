38ab12251607bd2e8e53bf8bcc1c6c6099d7f413
Dong Huynh <trungdong@donggiang.com>
2012-10-10 23:16:36 +0100

    Updated to use the latest RoundTripFromJava class from prov-xml

--- prov-json/src/test/java/org/openprovenance/prov/json/RoundTripFromJavaTest.java
+++ prov-json/src/test/java/org/openprovenance/prov/json/RoundTripFromJavaTest.java
@@ -2,21 +2,14 @@ package org.openprovenance.prov.json;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
-import java.io.IOException;
-
-import javax.xml.bind.JAXBException;
 
 import org.openprovenance.prov.xml.Document;
-import org.openprovenance.prov.xml.DocumentEquality;
-import org.openprovenance.prov.xml.Statement;
+import org.openprovenance.prov.xml.UncheckedTestException;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonIOException;
-import com.google.gson.JsonSyntaxException;
 
 /**
  * Round trip testing JavaBean -> PROV-JSON -> JavaBean
@@ -37,36 +30,31 @@ public class RoundTripFromJavaTest extends org.openprovenance.prov.xml.RoundTrip
 	public String extension() {
 		return ".json";
 	}
+	
+	public boolean checkTest(String name) {
+		return !(name.contains("member2") || name.contains("member3"));
+	}
 
+	
 	@Override
-	public void makeDocAndTest(Statement stment, String file, Statement[] opt, boolean check)
-			throws JAXBException {
-		Document d1 = pFactory.newDocument();
-		d1.getEntityOrActivityOrWasGeneratedBy().add(stment);
-		updateNamespaces(d1);
-		file = file + extension();
-
+	public Document readDocument(String file) {
 		try {
-			writeJSONDocument(d1, file);
-			Document d2 = readJSONDocument(file);
-			if (check) {
-				assertTrue(DocumentEquality.check(d1, d2));
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			Document doc = gson.fromJson(new BufferedReader(new FileReader(file)), Document.class);
+			return doc;
+		} catch (Exception e) {
+			throw new UncheckedTestException(e);
 		}
 	}
-	
-	protected Document readJSONDocument(String file) throws JsonSyntaxException, JsonIOException, FileNotFoundException {
-		Document doc = gson.fromJson(new BufferedReader(new FileReader(file)), Document.class);
-		return doc;
-	}
-	
-	protected void writeJSONDocument(Document doc, String file) throws IOException {
-		BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-		gson.toJson(doc, writer);
-		writer.close();
+
+	@Override
+	public void writeDocument(Document doc, String file) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			gson.toJson(doc, writer);
+			writer.close();
+		} catch (Exception e) {
+			throw new UncheckedTestException(e);
+		}
 	}
 
 }

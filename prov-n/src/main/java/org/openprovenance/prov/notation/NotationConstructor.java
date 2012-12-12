233c92083dbe5895d1175c75bad8538ad76a1020
Luc Moreau <luc.moreau@uprovenance.com>
2012-12-12 13:40:22 +0000

    new prov-n converter (issue #43)

--- prov-n/src/main/java/org/openprovenance/prov/notation/NotationConstructor.java
+++ prov-n/src/main/java/org/openprovenance/prov/notation/NotationConstructor.java
@@ -18,7 +18,6 @@ import org.openprovenance.prov.xml.BeanConstructor;
 import org.openprovenance.prov.xml.Document;
 import org.openprovenance.prov.xml.Entity;
 import org.openprovenance.prov.xml.HadMember;
-import org.openprovenance.prov.xml.InternationalizedString;
 import org.openprovenance.prov.xml.MentionOf;
 import org.openprovenance.prov.xml.NamedBundle;
 import org.openprovenance.prov.xml.ProvFactory;
@@ -286,6 +285,22 @@ public class NotationConstructor implements BeanConstructor {
         }
         return s;
     }
+    
+
+
+    public Object convertNamespace(Object pre, Object iri) {
+        return keyword("prefix") + " " + showprefix((String)pre) + " " + showuri((String)iri);
+    }
+
+    public Object convertDefaultNamespace(Object iri) {
+        return  keyword("default") + " " + showuri((String)iri);
+    }
+
+
+    public String showuri(String s) {
+        return  s;
+    }
+
 
     
     @Override
@@ -339,397 +354,35 @@ public class NotationConstructor implements BeanConstructor {
 	return sb.toString();
     }
      
-    private String oldOptionalAttributes(Object attrs) {
-        String s_attrs=(String)attrs;
-        if ("".equals(s_attrs)) {
-            return "";
-        } else {
-            return symbol(",[") + attrs + symbol("]");
-        }
-    }
-    private String symbol(String s) {
+    public String symbol(String s) {
 	return s;
     }
-    private String showprefix(String s) {
+    public String showprefix(String s) {
 	return s;
     }
 
-
-    private String optionalTime(Object time) {
-        return ((time==null)? "" : (", " + time));
-    }            
-
-    private Object optional2(Object str) {
-        return ((str==null)? "" : str);
-    }
-
-
-    private Object oldOptional(Object str) {
-        return ((str==null)? MARKER : str);
-    }
-
-    private Object convertActivity(Object id,Object startTime,Object endTime, Object aAttrs) {
-        String s=keyword("activity") + "(" + id + "," + oldOptional(startTime) + "," + oldOptional(endTime) + oldOptionalAttributes(aAttrs) + ")";
-        return s;
-    }
-    private Object convertEntity(Object id, Object attrs) {
-        String s=keyword("entity") + "(" + id  + oldOptionalAttributes(attrs) + ")";
-        return s;
-    }
-    private Object convertAgent(Object id, Object attrs) {
-        String s=keyword("agent") + "(" + id  + oldOptionalAttributes(attrs) + ")";
-        return s;
-    }
-    private String keyword(String s) {
-        return s;
-    }
-    private String breakline() {
-        return "\n";
-    }
-    private String showuri(String s) {
-        return  s;
-    }
-
-    private Object convertDocument(Object namespaces, List<Object> records,
-                                  List<Object> bundles) {
-        String s = keyword("document") + breakline();
-        if (namespaces != null) {
-            if (namespaces instanceof Hashtable) {
-                Hashtable<String, String> nss = (Hashtable<String, String>) namespaces;
-                // FIXME TODO: Should not be getting blank keys here.
-                if (nss.containsKey("")) {
-                    nss.put("_", nss.get(""));
-                    nss.remove("");
-                }
-                String def;
-                if ((def=nss.get("_"))!=null) {
-                    s = s + convertDefaultNamespace("<" + def + ">") + breakline();
-                }
-                 
-                for (String key : nss.keySet()) {
-                    String uri = nss.get(key);
-                    if (key.equals("_")) {
-                       // IGNORE, we have just handled it
-                    } else {
-                        s = s + convertNamespace(key, "<" + uri + ">")
-                                + breakline();
-                    }
-                }
-
-            } else {
-                s = s + namespaces + breakline();
-            }
-        }
-        for (Object o : records) {
-            s = s + o + breakline();
-        }
-        if (bundles != null) {
-            for (Object o : bundles) {
-                s = s + o + breakline();
-            }
-        }
-        s = s + keyword("endDocument");
-        return s;
-    }
-
-    private Object convertBundle(Object id, Object namespaces, List<Object> records) {
-        String s="bundle " + id + breakline();
-	if (namespaces!=null) {
-	    s=s+namespaces + breakline();
-	}
-	if (records!=null) 
-	    for (Object o: records) {
-		s=s+o+breakline();
-	    }
-        s=s+"endBundle";
-        return s;
-    }
-
-    private void oldStartBundle(Object bundleId) {
-    }
-
-    private Object convertAttributes(List<Object> attributes) {
-        String s="";
-        boolean first=true;
-        for (Object o: attributes) {
-            if (first) {
-                first=false;
-                s=s+o;
-            } else {
-                s=s+symbol(",")+ " " +o;
-            }
-        }
-        return s;
-    }
-    private Object convertId(String id) {
-        return id;
-    }
-
-
-    private Object convertAttribute(Object name, Object value) {
-	return name + "=" + value;
-    }
-    private Object convertStart(String start) {
-        return start;
-    }
-    private Object convertEnd(String end) {
-        return end;
-    }
-    private Object convertString(String s) {
-        return s;
-    }
-
-    private Object convertString(String s, String lang) {
-        return s + "@" + lang;
-    }
-
-    private Object convertInt(int i) {
-        return i;
-    }
-
-    private String oldOptionalId(Object id) {
-        return ((id==null)? "" : (id + ";"));
-    }            
-
-    private Object convertUsed(Object id, Object id2,Object id1, Object time, Object aAttrs) {
-        String s=keyword("used") + "(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1) + "," +
-                oldOptional(time) + oldOptionalAttributes(aAttrs) + ")";
-        return s;
-    }
-    private Object convertWasGeneratedBy(Object id, Object id2,Object id1, Object time, Object aAttrs ) {
-        String s=keyword("wasGeneratedBy") + "(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1) + "," +
-                oldOptional(time) + oldOptionalAttributes(aAttrs) +  ")";
-        return s;
-    }
-    private Object convertWasStartedBy(Object id, Object id2,Object id1, Object id3, Object time, Object aAttrs ) {
-        String s="wasStartedBy(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1) + "," + oldOptional(id3) + "," +
-                oldOptional(time) + oldOptionalAttributes(aAttrs) +  ")";
-            return s;
-    }
-    private Object convertWasEndedBy(Object id, Object id2,Object id1, Object id3, Object time, Object aAttrs ) {
-        String s="wasEndedBy(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1) + "," + oldOptional(id3) + "," +
-            oldOptional(time) + oldOptionalAttributes(aAttrs) +  ")";
-        return s;
-    }
-
-    private Object convertWasInformedBy(Object id, Object id2, Object id1, Object aAttrs) {
-        String s="wasInformedBy(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1)
-                + oldOptionalAttributes(aAttrs) +  ")";
-        return s;
-    }
-
-
-    private Object convertWasInvalidatedBy(Object id, Object id2,Object id1, Object time, Object aAttrs ) {
-        String s=keyword("wasInvalidatedBy") + "(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1) + "," +
-            oldOptional(time) + oldOptionalAttributes(aAttrs) +  ")";
-        return s;
-    }
-
-
-    private Object convertWasAttributedTo(Object id, Object id2,Object id1, Object aAttrs ) {
-        String s=keyword("wasAttributedTo") + "(" + oldOptionalId(id) + oldOptional(id2) + ", " + oldOptional(id1) +
-                oldOptionalAttributes(aAttrs) +  ")";
-        return s;
-    }
-
-
-    private Object convertWasDerivedFrom(Object id, Object id2,Object id1, Object pe, Object g2, Object u1, Object aAttrs) {
-        String s=keyword("wasDerivedFrom") + "(" + oldOptionalId(id) + oldOptional(id2) + ", " + oldOptional(id1) + 
-                ((pe==null && g2==null && u1==null) ?
-                 "" : ", " + oldOptional(pe) + ", " + oldOptional(g2) + ", " + oldOptional(u1)) + oldOptionalAttributes(aAttrs) +  ")";
-        return s;
-    }
 
     
-    private Object convertWasInfluencedBy(Object id, Object id2, Object id1, Object dAttrs) {
-        String s="wasInfluencedBy(" + oldOptionalId(id) + oldOptional(id2) + ", " + oldOptional(id1) + oldOptionalAttributes(dAttrs) +  ")";
+    public String keyword(String s) {
         return s;
     }
-
-
-    private Object convertAlternateOf(Object id2,Object id1) {
-        String s="alternateOf(" + id2 + "," + id1 + ")";
-        return s;
+    public String breakline() {
+        return "\n";
     }
-
-    private Object convertSpecializationOf(Object id2, Object id1) {
-        String s="specializationOf(" + id2 + "," + id1 + ")";
-        return s;
-    }
-
-    private Object convertMentionOf(Object su, Object bu, Object ta) {
-        String s="mentionOf(" + su + ", " + bu + ", " + oldOptional(ta) + ")";
-        return s;
-    }
-
-    private Object convertWasAssociatedWith(Object id, Object id2,Object id1, Object pl, Object aAttrs) {
-        String s=keyword("wasAssociatedWith") + "(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1) + "," +
-                oldOptional(pl) +
-                oldOptionalAttributes(aAttrs) + ")";
-        return s;
-    }
-
-    private Object convertActedOnBehalfOf(Object id, Object id2,Object id1, Object a, Object aAttrs) {
-        String s=keyword("actedOnBehalfOf") + "(" + oldOptionalId(id) + oldOptional(id2) + "," + oldOptional(id1) + "," +
-                oldOptional(a) +
-                oldOptionalAttributes(aAttrs) + ")";
-            return s;
-    }
-
-    private Object convertHadMember(Object collection, Object entity) {
-        String s=keyword("hadMember") + "(" + oldOptional(collection) + "," + oldOptional(entity) + ")";
-	return s;
-    }
-
+    
+  
+   
 
     private Object convertExtension(Object name, Object id, Object args, Object dAttrs) {
 	System.out.println("Name @" + name);
 	System.out.println("Name @" + id);
 	System.out.println("Name @" + args);
 	System.out.println("Name @" + dAttrs);
-        String s=keyword((String)name) + "(" + oldOptionalId(id) + args +
-            oldOptionalAttributes(dAttrs) + ")";
-	return s;
+  //      String s=keyword((String)name) + "(" + oldOptionalId(id) + args +
+  //          oldOptionalAttributes(dAttrs) + ")";
+  //	return s;
+	return null;
     }
-
-    private Object convertQualifiedName(String qname) {	
-        return qname;
-    }
-    private Object convertIRI(String iri) {
-        return iri;
-    }
-    //TODO should not use xsd:QName
-
-    private Object convertTypedLiteral(String datatype, Object value) {
-        if ("xsd:QName".equals(datatype)) {
-            String val=(String)value;
-            return "'" + val.substring(1, val.length() -1 ) + "'";
-        } else {
-	    if ("prov:Qualified_Name".equals(datatype)) {
-		String val=(String)value;
-		return "'" + val.substring(1, val.length() -1 ) + "'";
-	    } else {
-		if (value instanceof InternationalizedString) {
-		    InternationalizedString is=(InternationalizedString) value;
-		    value=convertInternationalizedString(is);
-		}
-		return value + " %% " + datatype;
-	    }
-	}
-    }
-
-
-    private Object convertInternationalizedString (InternationalizedString is) {
-	String value;
-	String lang=is.getLang();
-	if (lang==null) {
-	    value= "\""+ is.getValue() + "\"";
-	} else { 
-	    value= "\""+ is.getValue()+"\""+"@"+lang;
-	}
-	return value;
-    }
-
-
-   private Object convertNamespace(Object pre, Object iri) {
-       return keyword("prefix") + " " + showprefix((String)pre) + " " + showuri((String)iri);
-   }
-
-   private Object convertDefaultNamespace(Object iri) {
-       return  keyword("default") + " " + showuri((String)iri);
-   }
-
-    private Object convertNamespaces(List<Object> namespaces) {
-        String s="";
-        for (Object o: namespaces) {
-            s=s+o+breakline();
-        }
-        return s;
-    }
-
-    private Object convertPrefix(String pre) {
-        return pre;
-    }
-
-    /* Component 5 */
-
-    private Object convertInsertion(Object id, Object id2, Object id1, Object kes, Object iAttrs) {
-        String s="derivedByInsertionFrom(" + oldOptionalId(id) + id2 + ", " + id1 + ", "
-	    + kes + oldOptionalAttributes(iAttrs) +  ")";
-	return s;
-    }
-
-    private Object convertRemoval(Object id, Object id2, Object id1, Object keyset, Object rAttrs) {
-        String s="derivedByRemovalFrom(" + oldOptionalId(id) + id2 + ", " + id1 + ", "
-	    + keyset + oldOptionalAttributes(rAttrs) +  ")";
-	return s;
-
-    }
-
-    private Object convertCollectionMemberOf(Object id, Object id2, Object es, Object complete, Object mAttrs) {
-
-        String s="memberOf(" + oldOptionalId(id) + id2 + ", "
-	    + es + ((complete==null)? "" : ", "+complete) + oldOptionalAttributes(mAttrs) +  ")";
-	return s;
-
-    }
-
-
-    private Object convertDictionaryMemberOf(Object id, Object id2, Object kes, Object complete, Object mAttrs) {
-
-        String s="memberOf(" + oldOptionalId(id) + id2 + ", "
-	    + kes + ((complete==null)? "" : ", "+complete) + oldOptionalAttributes(mAttrs) +  ")";
-	return s;
-
-    }
-
-    private Object convertEntry(Object o1, Object o2) {
-        String s="{" + o1 + ", " + o2 + "}";
-	return s;
-    }
-
-    private Object convertKeyEntitySet(List<Object> entries) {
-        String s="{";
-
-	boolean first=true;
-
-	for (Object entry: entries) {
-	    if (!first) {
-		s=s+", ";
-	    } else {
-		first=false;
-	    }
-
-
-	    s=s + entry;
-	}
-	s=s+"}";
-	return s;
-    }
-
-
-    private Object convertKeys(List<Object> keys) {
-        String s="{";
-
-	boolean first=true;
-
-	for (Object key: keys) {
-	    if (!first) {
-		s=s+", ";
-	    } else {
-		first=false;
-	    }
-
-
-	    s=s + key;
-	}
-	s=s+"}";
-	return s;
-    }
-
-
-
-    /* Component 6 */
 
 
 

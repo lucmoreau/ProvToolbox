4c81dde4321765dc88d8478439fde049a9b5d33f
Luc Moreau <l.moreau@ecs.soton.ac.uk>
2012-12-14 12:02:41 +0000

    rdf conversion (issue #45)

---
+++ prov-rdf/src/test/java/org/openprovenance/prov/rdf/RoundTripFromJavaWithElmoTest.java
@@ -0,0 +1,202 @@
+package org.openprovenance.prov.rdf;
+
+import java.io.StringWriter;
+
+import javax.xml.bind.JAXBException;
+
+import org.openprovenance.prov.xml.Document;
+import org.openprovenance.prov.xml.HasExtensibility;
+import org.openprovenance.prov.xml.ProvFactory;
+import org.openprovenance.prov.xml.ProvSerialiser;
+import org.openprovenance.prov.xml.UncheckedTestException;
+import org.openrdf.rio.RDFFormat;
+
+public class RoundTripFromJavaWithElmoTest extends
+		org.openprovenance.prov.xml.RoundTripFromJavaTest
+{
+
+	public RoundTripFromJavaWithElmoTest(String name)
+	{
+		super(name);
+	}
+
+	public String extension()
+	{
+		return ".trig";
+	}
+
+	@Override
+	public Document readDocument(String file)
+	{
+		try
+		{
+			Document doc2 = u.parseRDF(file);
+			return doc2;
+		} catch (Exception e)
+		{
+			throw new UncheckedTestException(e);
+		}
+	}
+
+	@Override
+	public void writeDocument(Document doc, String file)
+	{
+		try
+		{
+			u.dumpRDFWithElmo(pFactory, doc, RDFFormat.TRIG, file);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+			throw new UncheckedTestException(e);
+		}
+	}
+
+	@Override
+	public boolean checkTest(String name)
+	{
+		if (name.endsWith("-S" + extension()))
+		{
+			return false;
+		}
+		return true;
+	}
+
+	final Utility u = new Utility();
+
+	private String dumpXML(ProvFactory pFactory, Document document)
+			throws JAXBException
+	{
+
+		ProvSerialiser serial = ProvSerialiser.getThreadProvSerialiser();
+		StringWriter sw = new StringWriter();
+		serial.serialiseDocument(sw, document, true);
+		return sw.toString();
+	}
+	
+	@Override
+	public void addFurtherAttributesWithQNames(HasExtensibility he) {
+	    //TODO: qnames not supported here, yet
+	}
+
+
+	public boolean mergeDuplicateProperties()
+	{
+		return true;
+	}
+
+	public void testInfluence1() throws JAXBException
+	{
+		// Class cast errors (ActivityOrAgentOrEntity)   Limitation of Elmo, I think this can be fixed by asserting triples without Elmo
+	}
+
+	public void testInfluence2() throws JAXBException
+	{
+		// Class cast errors (ActivityOrAgentOrEntity)    Limitation of Elmo, I think this can be fixed by asserting triples without Elmo
+	}
+
+	public void testInfluence3() throws JAXBException
+	{
+		// Class cast errors (ActivityOrAgentOrEntity)
+	}
+
+	public void testInfluence4() throws JAXBException
+	{
+		// Class cast errors (ActivityOrAgentOrEntity)
+	}
+
+	public void testInfluence5() throws JAXBException
+	{
+		// Class cast errors (ActivityOrAgentOrEntity)
+	}
+
+	public void testInfluence6() throws JAXBException
+	{
+		// Class cast errors (ActivityOrAgentOrEntity)
+	}
+
+	public void testInfluence7() throws JAXBException
+	{
+		// Class cast errors (ActivityOrAgentOrEntity)
+	}
+
+	
+	public void testMembership1() throws JAXBException
+	{
+	    
+		// TODO: RDF Collector doesn't support hadMember
+	}
+
+	public void testMembership2() throws JAXBException
+	{
+		// TODO: Unsupported, no multiple members supported by toolbox
+	}
+
+	public void testMembership3() throws JAXBException
+	{
+		// TODO: Unsupported, no multiple members supported by toolbox
+	}
+	
+    public void testBundle2() {
+		// TODO: Not supported yet, though seemed to work at some point?
+    }
+	
+	public void testScruffyGeneration1() {
+		// TODO: Unsupported
+
+	}
+	  public void testScruffyGeneration2(){
+			// TODO: Unsupported  
+	  }
+	  public void testScruffyInvalidation1(){
+			// TODO: Unsupported  
+	  }
+	  public void testScruffyInvalidation2() {
+			// TODO: Unsupported  
+	  }
+	  public void testScruffyUsage1() {
+			// TODO: Unsupported  
+	  }
+	  public void testScruffyUsage2() {
+			// TODO: Unsupported    
+	  }
+	  public void testScruffyStart1() {
+			// TODO: Unsupported    		  
+	  }
+	  public void testScruffyStart2() {
+			// TODO: Unsupported    		  
+	  }
+	  public void testScruffyStart3() {
+			// TODO: Unsupported    		  
+	  }
+	  public void testScruffyStart4() {
+			// TODO: Unsupported    		  
+	  }
+	  public void testScruffyEnd1() {
+			// TODO: Unsupported    		  
+	  }
+	  public void testScruffyEnd2() {
+			// TODO: Unsupported    		  
+	  }
+	  public void testScruffyEnd3() {
+			// TODO: Unsupported    		  
+	  }
+	  public void testScruffyEnd4() {
+			// TODO: Unsupported    		  
+	  }
+
+	  
+	 
+
+	public void IGNOREtestExtraFilesOutsideRepository()
+	{
+		Document doc = readDocument("/home/lavm/Downloads/index-cloudsat_airs.aqua-v3.1-2006.09.01.001429.rdf");
+		try
+		{
+			System.out.println(" xml " + dumpXML(pFactory, doc));
+		} catch (JAXBException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}

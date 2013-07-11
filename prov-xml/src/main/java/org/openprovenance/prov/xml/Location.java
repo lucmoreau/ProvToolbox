88c6e9a71d030c82d0e4c853587a90c1f4b5deec
Luc Moreau <luc.moreau@uprovenance.com>
2013-07-11 09:30:05 +0100

    added location and location adapter

---
+++ prov-xml/src/main/java/org/openprovenance/prov/xml/Location.java
@@ -0,0 +1,62 @@
+package org.openprovenance.prov.xml;
+
+
+import java.util.Collection;
+
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import javax.xml.namespace.QName;
+
+
+@XmlJavaTypeAdapter(LocationAdapter.class)
+public class Location {
+    
+    private Object val;
+    private QName xsdType;
+
+    
+    public Location() {
+    }
+    
+    public Location(Object val, QName xsdType) {
+ 	this.val = val;
+ 	this.xsdType = xsdType;
+     }
+
+   
+    public boolean equals(Object o) {
+	if (o instanceof Location) {
+	    Location other = (Location) o;
+	    return val.equals(other.val) && xsdType.equals(other.xsdType);
+
+	}
+	return false;
+    }
+
+
+    public QName getXsdType() {
+	return xsdType;
+    }
+
+
+    public Object getValue() {
+	return val;
+    }
+    
+
+    @Override
+    public int hashCode() {
+	int hash = 0;
+	if (val != null)
+	    hash ^= val.hashCode();
+	if (xsdType != null)
+	    hash ^= xsdType.hashCode();
+	return hash;
+    }
+
+
+    public String toStringDebug() { 
+	return "[loc " + val + " " + xsdType + "]";
+    }
+
+
+}

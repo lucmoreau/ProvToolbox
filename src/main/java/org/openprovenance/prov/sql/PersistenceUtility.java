0a81b3378ae8511f28de8b04b3373e6dfaefd0d3
Luc Moreau <luc.moreau@uprovenance.com>
2013-07-30 16:33:16 +0100

    support for persistence

---
+++ src/main/java/org/openprovenance/prov/sql/PersistenceUtility.java
@@ -0,0 +1,69 @@
+package org.openprovenance.prov.sql;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+
+import org.apache.log4j.Logger;
+import org.openprovenance.prov.xml.Document;
+
+public class PersistenceUtility {
+    static Logger logger = Logger.getLogger(PersistenceUtility.class);
+    final private EntityManagerFactory emf;                                                                                                                   
+
+    public PersistenceUtility() {
+        this.entityManager=createEntityManager();          
+        Map<String,String> properties=makeProperties(getUnitName());
+        
+        this.emf=javax.persistence.Persistence.createEntityManagerFactory(getUnitName(),                                                                                                 
+                                                                          properties);       
+
+    }
+             
+    String getUnitName() {
+        return "org.openprovenance.prov.sql";
+    }
+
+    final private EntityManager entityManager;      
+    
+    public EntityManager createEntityManager () {                                                                                                                                        
+        EntityManager em=emf.createEntityManager();                                                                                                                                      
+        return em;                                                                                                                                                                       
+    }                                                                                                                                                                                    
+                                                                                                                                                                                         
+    static Map<String,String> makeProperties(String filename) {                                                                                                                          
+        Map<String,String> properties=new HashMap<String, String>();                                                                                                                     
+        properties.put("hibernate.ejb.cfgfile",filename);                                                                                                                                
+        properties.put("javax.persistence.provider","org.hibernate.ejb.HibernatePersistence");                                                                                           
+        logger.debug(properties);                                                                                                                                                        
+        return properties;                                                                                                                                                               
+    }                                                                                                                                                                                    
+   
+    
+    public EntityTransaction getTransaction() {                                             
+        return entityManager.getTransaction();                                              
+    }
+                                                                           
+    
+    public EntityTransaction beginTransaction() {                                                                                                                                        
+        EntityTransaction tx=entityManager.getTransaction();                                                                                                                             
+        tx.begin();                                                                                                                                                                      
+        return tx;                                                                                                                                                                       
+    }                                                                                                                                                                                    
+             
+    public void close() {                                                                                                                                                                
+        entityManager.close();                                                                                                                                                           
+    }          
+    
+
+    public Document persist(Document doc) {
+        beginTransaction();
+        entityManager.persist(doc);
+        close();
+        return doc;
+    }            
+
+}

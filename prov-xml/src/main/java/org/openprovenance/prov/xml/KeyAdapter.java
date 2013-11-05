97ca63c298b90660ae477458a5fa5d4f9f102eb1
Luc Moreau <luc.moreau@uprovenance.com>
2013-11-05 12:24:58 +0000

    revisiting marshalling of keys in dictionary

--- prov-xml/src/main/java/org/openprovenance/prov/xml/KeyAdapter.java
+++ prov-xml/src/main/java/org/openprovenance/prov/xml/KeyAdapter.java
@@ -1,18 +1,29 @@
 package org.openprovenance.prov.xml;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import org.w3c.dom.Element;
 
-public class KeyAdapter extends XmlAdapter<Object, TypedValue> {
+public class KeyAdapter extends XmlAdapter<Element, TypedValue> {
+
+    final org.openprovenance.prov.model.ProvFactory pFactory;
+    final ValueConverter vconv;
+
+    public KeyAdapter() {
+	pFactory= new ProvFactory();
+	vconv=new ValueConverter(pFactory);
+    }
+
+
 
     @Override
-    public Object marshal(TypedValue value) throws Exception {
+    public Element marshal(TypedValue value) throws Exception {
          System.out.println("==> KeyAdapter marshalling for " + value);
 	// TODO Auto-generated method stub
 	return null;
     }
 
     @Override
-    public TypedValue unmarshal(Object value) throws Exception {
+    public TypedValue unmarshal(Element value) throws Exception {
          System.out.println("==> KeyAdapter unmarshalling for " + value);
 	// TODO Auto-generated method stub
 	return null;

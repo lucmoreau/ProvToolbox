267e8d8abbeb6246347f0a59e18737481885a322
Luc Moreau <luc.moreau@uprovenance.com>
2013-12-11 23:38:27 +0000

    changed constant names, LangString, and added some javadoc

--- prov-model/src/main/java/org/openprovenance/prov/model/Element.java
+++ prov-model/src/main/java/org/openprovenance/prov/model/Element.java
@@ -1,10 +1,13 @@
 package org.openprovenance.prov.model;
 
-/** A Java class for an Entity, an Activity, or an Agent.
- * <p>An element can have a type ({@link HasType}), can have a label ({@link HasLabel), 
- * can have a location ({@link HasLocation), is {@link Identifiable}, and is one of the assertions supported by PROV {@link Statement}.
+/** 
+ * <p>A Java class for an Entity, an Activity, or an Agent.
+ * <p>An element can have a type ({@link HasType}), can have a label ({@link HasLabel}), 
+ * can have a location ({@link HasLocation}), is {@link Identifiable}, and is one of 
+ * the assertions supported by PROV ({@link Statement}).
  * 
- *  @author lavm*/ 
+ *  @author lavm
+ */ 
 public interface Element extends HasType, HasLabel, HasLocation, Identifiable,  Statement {
     
 } 

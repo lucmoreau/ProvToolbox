533434a3ea43d9d79fce2ae8f77496b5f61147dd
Luc Moreau <luc.moreau@uprovenance.com>
2013-12-12 21:09:42 +0000

    javadoc

--- prov-model/src/main/java/org/openprovenance/prov/model/WasAttributedTo.java
+++ prov-model/src/main/java/org/openprovenance/prov/model/WasAttributedTo.java
@@ -15,12 +15,29 @@ package org.openprovenance.prov.model;
  */
 public interface WasAttributedTo  extends Identifiable,  HasLabel, HasType, HasOther, Influence {
 
-    void setEntity(QualifiedName eid);
+    /** Set  the entity identifier.
+     * 
+     * @param entity {@link QualifiedName} of the entity
+     */
+    
+    void setEntity(QualifiedName entity);
 
-    void setAgent(QualifiedName agid);
+    /**  Set the identifier of the agent whom the entity is ascribed to.
+     * 
+     * @param agent {@link QualifiedName} of the agent
+     */
+    void setAgent(QualifiedName agent);
 
+    /** Gets the entity identifier.
+     * 
+     * @return {@link QualifiedName} of the entity
+     */
     QualifiedName getEntity();
 
+    /** Get the identifier of the agent whom the entity is ascribed to, and therefore 
+     * bears some responsibility for its existence
+     * @return {@link QualifiedName} of the agent.
+     */
     QualifiedName getAgent();
 
 }

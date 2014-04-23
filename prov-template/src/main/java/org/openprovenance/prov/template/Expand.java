3c23e1d8cbbfb045cd5a2f823a911a286ee74032
Luc Moreau <luc.moreau@uprovenance.com>
2014-04-23 22:04:39 +0100

    template system

---
+++ prov-template/src/main/java/org/openprovenance/prov/template/Expand.java
@@ -0,0 +1,12 @@
+package org.openprovenance.prov.template;
+
+import org.openprovenance.prov.model.Document;
+
+public class Expand {
+    Document expand(Document template,
+                    Object bindings) {
+	return null;
+	
+    }
+
+}

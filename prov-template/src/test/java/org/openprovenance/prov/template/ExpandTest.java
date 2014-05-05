156186cdc687087b9f0dacd9a97ad9a3b514f908
Luc Moreau <luc.moreau@uprovenance.com>
2014-05-05 16:23:57 +0100

    expansion mechanism

--- prov-template/src/test/java/org/openprovenance/prov/template/ExpandTest.java
+++ prov-template/src/test/java/org/openprovenance/prov/template/ExpandTest.java
@@ -31,82 +31,11 @@ public class ExpandTest extends TestCase {
     QualifiedName var_d=pf.newQualifiedName(VAR_NS, "d", "var");
     QualifiedName var_e=pf.newQualifiedName(VAR_NS, "e", "var");
  
-    public void NOtestBinding1() {
-	Bindings bindings1=new Bindings();
 
-	bindings1.addVariable(var_a,
-	                      pf.newQualifiedName(EX_NS, "av1", "ex"));
-	bindings1.addVariable(var_a,
-	                      pf.newQualifiedName(EX_NS, "av2", "ex"));
-	bindings1.addVariable(var_a,
-	                      pf.newQualifiedName(EX_NS, "av3", "ex"));
-	
-	bindings1.addVariable(var_b,
-	                      pf.newQualifiedName(EX_NS, "bv1", "ex"));
-	bindings1.addVariable(var_b,
-	                      pf.newQualifiedName(EX_NS, "bv2", "ex"));
-	
-	System.out.println(bindings1);
-	
-
-	Groupings grp1=new Groupings();
-	grp1.addVariable(var_a);
-	grp1.addVariable(var_b);
-	System.out.println(grp1);
-	
-
-	
-
-	Groupings grp2=new Groupings();
-	grp2.addVariable(var_a);
-	grp2.addVariable(0,var_b);
-	System.out.println(grp2);
-
-
-	Groupings grp3=new Groupings();
-	grp3.addVariable(var_a);
-	grp3.addVariable(0,var_b);
-	grp3.addVariable(var_c);
-
-	System.out.println(grp3);
-
-
-
-	Using us1=new Using();
-	us1.addGroup(1, 2);
-	us1.addGroup(2, 3);
-        System.out.println(us1);
-        assertTrue(us1.checkIndex(Arrays.asList(new Integer[]{0,0})));
-        assertTrue(us1.checkIndex(Arrays.asList(new Integer[]{1,2})));
-        assertFalse(us1.checkIndex(Arrays.asList(new Integer[]{1,3})));
-        assertFalse(us1.checkIndex(Arrays.asList(new Integer[]{2,2})));
-        assertFalse(us1.checkIndex(Arrays.asList(new Integer[]{2,2,4})));
-        assertFalse(us1.checkIndex(Arrays.asList(new Integer[]{2})));
-        
-        assertEquals(us1.nextIndex(Arrays.asList(new Integer[]{0,0})),
-                     Arrays.asList(new Integer[]{1,0}));
-        assertEquals(us1.nextIndex(Arrays.asList(new Integer[]{1,0})),
-                     Arrays.asList(new Integer[]{0,1}));
-        assertEquals(us1.nextIndex(Arrays.asList(new Integer[]{0,1})),
-                     Arrays.asList(new Integer[]{1,1}));
-        assertEquals(us1.nextIndex(Arrays.asList(new Integer[]{1,1})),
-                     Arrays.asList(new Integer[]{0,2}));
-        assertNull(us1.nextIndex(Arrays.asList(new Integer[]{1,2})));
-        
-        Iterator<List<Integer>> it=us1.iterator();
-        
-        while (it.hasNext()) {
-            System.out.println("next is " + it.next());
-        }
-        
-
-	
-    }
 
     public void expander (String in,
                           String out,
                           Bindings bindings1,
-                          Groupings grp1,       
                           String outBindings) {
 	System.out.println("expander ==========================================> " + in);
 	
@@ -114,8 +43,9 @@ public class ExpandTest extends TestCase {
 	
 	NamedBundle bun=(NamedBundle) doc.getStatementOrBundle().get(0);
 
-	
+	Groupings grp1=Groupings.fromDocument(doc);
 
+	System.out.println("Found groupings " + grp1);
 	
 	NamedBundle bun1=(NamedBundle) new Expand().expand(bun, bindings1, grp1).get(0);
 	Document doc1=pf.newDocument();
@@ -155,18 +85,11 @@ public class ExpandTest extends TestCase {
 	                      pf.newQualifiedName(EX_NS, "bv1", "ex"));
 	bindings1.addVariable(var_b,
 	                      pf.newQualifiedName(EX_NS, "bv2", "ex"));
-	//System.out.println(bindings1);
 
-	
-	Groupings grp1=new Groupings();
-	grp1.addVariable(var_a);
-	grp1.addVariable(var_b);
-	//System.out.println(grp1);
 	
 	expander("src/test/resources/template1.provn",
 	         "target/expanded1.provn",
 	         bindings1,
-	         grp1,
 	         "target/bindings1.provn");
 	
 	
@@ -185,18 +108,11 @@ public class ExpandTest extends TestCase {
 	                      pf.newQualifiedName(EX_NS, "bv1", "ex"));
 	bindings1.addVariable(var_b,
 	                      pf.newQualifiedName(EX_NS, "bv2", "ex"));
-	//System.out.println(bindings1);
 
 	
-	Groupings grp1=new Groupings();
-	grp1.addVariable(var_a);
-	grp1.addVariable(0,var_b);
-	//System.out.println(grp1);
-	
-	expander("src/test/resources/template1.provn",
+	expander("src/test/resources/template2.provn",
 	         "target/expanded2.provn",
 	         bindings1,
-	         grp1,
 	         "target/bindings2.provn");
 	
 	
@@ -223,106 +139,17 @@ public class ExpandTest extends TestCase {
  	bindings1.addVariable(var_c,
  	                      pf.newQualifiedName(EX_NS, "cv2", "ex"));
  	
- 	//System.out.println(bindings1);
-
- 	
- 	Groupings grp1=new Groupings();
- 	grp1.addVariable(var_a);
- 	grp1.addVariable(var_b);
- 	grp1.addVariable(1,var_c);
- 	//System.out.println(grp1);
- 	
- 	expander("src/test/resources/template2.provn",
+  	
+ 	expander("src/test/resources/template3.provn",
  	         "target/expanded3.provn",
  	         bindings1,
- 	         grp1,
  	        "target/bindings3.provn");
  	
  	
      }
     
 
-    public void testExpand10() {
-        
-        Bindings bindings1=new Bindings();
-
-        bindings1.addVariable(var_a,
-                              pf.newQualifiedName(EX_NS, "av1", "ex"));
-        bindings1.addVariable(var_a,
-                              pf.newQualifiedName(EX_NS, "av2", "ex"));
-        bindings1.addVariable(var_a,
-                              pf.newQualifiedName(EX_NS, "av3", "ex"));
-        
-        bindings1.addVariable(var_b,
-                              pf.newQualifiedName(EX_NS, "bv1", "ex"));
-        bindings1.addVariable(var_b,
-                              pf.newQualifiedName(EX_NS, "bv2", "ex"));
-        
-        bindings1.addVariable(var_c,
-                              pf.newQualifiedName(EX_NS, "cv1", "ex"));
-        bindings1.addVariable(var_c,
-                              pf.newQualifiedName(EX_NS, "cv2", "ex"));
-        
-        
-        //System.out.println(bindings1);
-
-        
-        Groupings grp1=new Groupings();
-        grp1.addVariable(var_a);
-        grp1.addVariable(var_b);
-        grp1.addVariable(1,var_c);
-        //System.out.println(grp1);
-        
-        expander("src/test/resources/template10.provn",
-                 "target/expanded10.provn",
-                 bindings1,
-                 grp1,
-                 "target/bindings10.provn");
-        
-        
-    }
     
-    
-
-    public void testExpand11() {
-        
-        Bindings bindings1=new Bindings();
-
-        bindings1.addVariable(var_a,
-                              pf.newQualifiedName(EX_NS, "av1", "ex"));
-        bindings1.addVariable(var_a,
-                              pf.newQualifiedName(EX_NS, "av2", "ex"));
-        bindings1.addVariable(var_a,
-                              pf.newQualifiedName(EX_NS, "av3", "ex"));
-        
-        bindings1.addVariable(var_b,
-                              pf.newQualifiedName(EX_NS, "bv1", "ex"));
-        bindings1.addVariable(var_b,
-                              pf.newQualifiedName(EX_NS, "bv2", "ex"));
-        
-        bindings1.addVariable(var_c,
-                              pf.newQualifiedName(EX_NS, "cv1", "ex"));
-        bindings1.addVariable(var_c,
-                              pf.newQualifiedName(EX_NS, "cv2", "ex"));
-        
-        
-        //System.out.println(bindings1);
-
-        
-        Groupings grp1=new Groupings();
-        grp1.addVariable(var_a);
-        grp1.addVariable(var_b);
-        grp1.addVariable(var_c);
-        //System.out.println(grp1);
-        
-        expander("src/test/resources/template10.provn",
-                 "target/expanded11.provn",
-                 bindings1,
-                 grp1,
-                 "target/bindings11.provn");
-        
-        
-    }
     
     public void testExpand20() {
         
@@ -383,22 +210,140 @@ public class ExpandTest extends TestCase {
       
         
         
-        //System.out.println(bindings1);
-
-        
-        Groupings grp1=new Groupings();
-        grp1.addVariable(var_a);
-        grp1.addVariable(var_b);
-        grp1.addVariable(1,var_c);
-        //grp1.addVariable(var_d);
-
-        //System.out.println(grp1);
-        
+  
         expander("src/test/resources/template20.provn",
                  "target/expanded20.provn",
                  bindings1,
-                 grp1,
                  "target/bindings20.provn");
+        
+        
+    }
+    
+    
+    
+    
+    public void testExpand21() {
+        
+        Bindings bindings1=new Bindings();
+
+        bindings1.addVariable(var_a,
+                              pf.newQualifiedName(EX_NS, "av1", "ex"));
+        bindings1.addVariable(var_a,
+                              pf.newQualifiedName(EX_NS, "av2", "ex"));
+        bindings1.addVariable(var_a,
+                              pf.newQualifiedName(EX_NS, "av3", "ex"));
+        
+        bindings1.addVariable(var_b,
+                              pf.newQualifiedName(EX_NS, "bv1", "ex"));
+        bindings1.addVariable(var_b,
+                              pf.newQualifiedName(EX_NS, "bv2", "ex"));
+        
+        bindings1.addVariable(var_b,
+                              pf.newQualifiedName(EX_NS, "bv3", "ex"));
+        
+        
+        List<TypedValue> ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "me1@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_d, ll);
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "me2@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_d, ll);
+    
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "me3@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_d, ll);
+    
+
+        
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc1@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_e, ll);
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc2@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_e, ll);
+    
+      
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc6@example", pf.getName().XSD_STRING));
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc7@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_e, ll);
+      
+        
+        
+  
+        expander("src/test/resources/template21.provn",
+                 "target/expanded21.provn",
+                 bindings1,
+                 "target/bindings21.provn");
+        
+        
+    }
+    
+    
+    
+    
+    
+    public void testExpand22() {
+        
+        Bindings bindings1=new Bindings();
+
+        bindings1.addVariable(var_a,
+                              pf.newQualifiedName(EX_NS, "av1", "ex"));
+        bindings1.addVariable(var_a,
+                              pf.newQualifiedName(EX_NS, "av2", "ex"));
+        bindings1.addVariable(var_a,
+                              pf.newQualifiedName(EX_NS, "av3", "ex"));
+        
+        bindings1.addVariable(var_b,
+                              pf.newQualifiedName(EX_NS, "bv1", "ex"));
+        bindings1.addVariable(var_b,
+                              pf.newQualifiedName(EX_NS, "bv2", "ex"));
+        
+        bindings1.addVariable(var_b,
+                              pf.newQualifiedName(EX_NS, "bv3", "ex"));
+        
+        
+        List<TypedValue> ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "me1@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_d, ll);
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "me2@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_d, ll);
+    
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "me3@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_d, ll);
+    
+
+        
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc1@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_e, ll);
+
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc2@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_e, ll);
+    
+      
+        ll=new LinkedList<TypedValue>();
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc6@example", pf.getName().XSD_STRING));
+        ll.add(pf.newOther(pf.newQualifiedName(APP_NS, "ignore", "app"), "Luc7@example", pf.getName().XSD_STRING));
+        bindings1.addAttribute(var_e, ll);
+      
+        
+        
+  
+        expander("src/test/resources/template22.provn",
+                 "target/expanded22.provn",
+                 bindings1,
+                 "target/bindings22.provn");
         
         
     }

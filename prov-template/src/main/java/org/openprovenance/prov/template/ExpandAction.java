e34750c3cfefb2aaf500987a241b6111eac4543a
Luc Moreau <luc.moreau@uprovenance.com>
2014-06-04 22:50:33 +0100

    update imports

--- prov-template/src/main/java/org/openprovenance/prov/template/ExpandAction.java
+++ prov-template/src/main/java/org/openprovenance/prov/template/ExpandAction.java
@@ -1,6 +1,5 @@
 package org.openprovenance.prov.template;
 
-import java.nio.channels.UnsupportedAddressTypeException;
 import java.util.Collection;
 import java.util.Hashtable;
 import java.util.LinkedList;

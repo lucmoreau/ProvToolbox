261f6dd781418c5f5a9d67320c15474030059c31
Luc Moreau <luc.moreau@uprovenance.com>
2014-07-29 13:16:43 +0100

    formattign

--- prov-interop/src/main/java/org/openprovenance/prov/interop/InteropException.java
+++ prov-interop/src/main/java/org/openprovenance/prov/interop/InteropException.java
@@ -1,9 +1,9 @@
 package org.openprovenance.prov.interop;
 
+/** A Runtime Exception for the InteropFramework. */
+
 public class InteropException extends RuntimeException {
-    /**
-     * 
-     */
+
     private static final long serialVersionUID = 341614640704209138L;
 
     public InteropException(Exception e) {

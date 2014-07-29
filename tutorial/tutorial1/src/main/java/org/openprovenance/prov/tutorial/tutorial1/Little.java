0067a11e5208b13746c1f815b73f186ed975505c
Luc Moreau <luc.moreau@uprovenance.com>
2014-07-29 16:26:33 +0100

    tutorial 1

--- tutorial/tutorial1/src/main/java/org/openprovenance/prov/tutorial/tutorial1/Little.java
+++ tutorial/tutorial1/src/main/java/org/openprovenance/prov/tutorial/tutorial1/Little.java
@@ -1,85 +1,113 @@
 package org.openprovenance.prov.tutorial.tutorial1;
 
 import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import org.openprovenance.prov.model.Activity;
+
+import org.openprovenance.prov.interop.InteropFramework;
+import org.openprovenance.prov.interop.InteropFramework.ProvFormat;
 import org.openprovenance.prov.model.Agent;
 import org.openprovenance.prov.model.Document;
 import org.openprovenance.prov.model.Entity;
 import org.openprovenance.prov.model.Namespace;
 import org.openprovenance.prov.model.QualifiedName;
 import org.openprovenance.prov.model.ProvFactory;
-import org.openprovenance.prov.model.Statement;
 import org.openprovenance.prov.model.StatementOrBundle;
-import org.openprovenance.prov.model.Type;
-import org.openprovenance.prov.model.Used;
-import org.openprovenance.prov.model.WasAssociatedWith;
 import org.openprovenance.prov.model.WasAttributedTo;
 import org.openprovenance.prov.model.WasDerivedFrom;
-import org.openprovenance.prov.model.WasGeneratedBy;
 
-
-/** A little provenance goes a long way.
+/**
+ * A little provenance goes a long way. 
+ * ProvToolbox Tutorial 1: creating a provenance document in Java and serializing it
+ * to SVG (in a file) and to PROVN (on the console).
  * 
  * @author lavm
  * @see http://blog.provbook.org/2013/10/11/a-little-provenance-goes-a-long-way/
  */
 public class Little {
 
+    
     public static final String PROVBOOK_NS = "http://www.provbook.org";
     public static final String PROVBOOK_PREFIX = "provbook";
     
-    public static final String PRIM_NS = "http://openprovenance.org/primitives#";
-    public static final String PRIM_PREFIX = "prim";
-      
+    public static final String JIM_PREFIX = "jim";
+    public static final String JIM_NS = "http://www.cs.rpi.edu/~hendler/";
+
     private final ProvFactory pFactory;
-
+    private final Namespace ns;
     public Little(ProvFactory pFactory) {
-	this.pFactory=pFactory;
+        this.pFactory = pFactory;
+        ns=new Namespace();
+        ns.register(PROVBOOK_PREFIX, PROVBOOK_NS);
+        ns.register(JIM_PREFIX, JIM_NS);
+
     }
-        
+
+    public QualifiedName qn(String n) {
+        return ns.qualifiedName(PROVBOOK_PREFIX, n, pFactory);
+    }
+
+    public Document makeLittle() {     
+       
+        Entity quote = pFactory.newEntity(qn("a-little-provenance-goes-a-long-way"));
+        quote.setValue(pFactory.newValue("A little provenance goes a long way",
+                                         pFactory.getName().XSD_STRING));
+
+        Agent paul = pFactory.newAgent(qn("Paul"), "Paul Groth");
+        Agent luc = pFactory.newAgent(qn("Luc"), "Luc Moreau");
+
+        WasAttributedTo attr1 = pFactory.newWasAttributedTo(null,
+                                                            quote.getId(),
+                                                            paul.getId());
+        WasAttributedTo attr2 = pFactory.newWasAttributedTo(null,
+                                                            quote.getId(),
+                                                            luc.getId());
+
+        Entity original = pFactory.newEntity(ns.qualifiedName(JIM_PREFIX,"LittleSemanticsWeb.html",pFactory));
+
+        WasDerivedFrom wdf = pFactory.newWasDerivedFrom(quote.getId(),
+                                                        original.getId());
+
+        Document document = pFactory.newDocument();
+        document.getStatementOrBundle()
+                .addAll(Arrays.asList(new StatementOrBundle[] { quote, 
+                                                                paul,
+                                                                luc, 
+                                                                attr1,
+                                                                attr2, 
+                                                                original,
+                                                                wdf }));
+
+        document.setNamespace(Namespace.gatherNamespaces(document));
+
+        return document;
+    }
     
-    public QualifiedName q(String n) {
-   	//return new org.openprovenance.prov.xml.QualifiedName(PC1_NS, n, PC1_PREFIX);
-   	return pFactory.newQualifiedName(PROVBOOK_NS, n, PROVBOOK_PREFIX);
-    }
- 
- 
+    public void doConversions(Document document, String file) {
+        InteropFramework intF=new InteropFramework();
+        intF.writeDocument(file, document);     
+        intF.writeDocument(System.out, ProvFormat.PROVN, document);
 
-
-
-    public Document makeLittle(ProvFactory pFactory) {
-
-	
-
-	Entity quote = pFactory.newEntity(q("a-little-provenance-goes-a-long-way"));
-	quote.setValue(pFactory.newValue("A little provenance goes a long way", pFactory.getName().QNAME_XSD_HASH_STRING));
-	
-	Agent paul = pFactory.newAgent(q("Paul"), "Paul Groth");
-	Agent luc = pFactory.newAgent(q("Luc"), "Luc Moreau");
-
-	WasAttributedTo attr1=pFactory.newWasAttributedTo(null, quote.getId(), paul.getId());
-	WasAttributedTo attr2=pFactory.newWasAttributedTo(null, quote.getId(), luc.getId());
-
-	Entity original=pFactory.newEntity(pFactory.newQualifiedName("http://www.cs.rpi.edu/~hendler/", "LittleSemanticsWeb.html", "hendler"));
-	
-	WasDerivedFrom wdf=pFactory.newWasDerivedFrom(quote.getId(), original.getId());
-	
-		Document document = pFactory.newDocument();
-		document.getStatementOrBundle().addAll(Arrays.asList(new StatementOrBundle[] {quote, paul, luc, attr1, attr2, original, wdf}));
-		
-	document.setNamespace(Namespace.gatherNamespaces(document));
-
-	return document;
     }
 
+    public void closingBanner() {
+        System.out.println("");
+        System.out.println("*************************");
+    }
 
+    public void openingBanner() {
+        System.out.println("*************************");
+        System.out.println("* Converting document  ");
+        System.out.println("*************************");
+    }
+    
+    public static void main(String [] args) {
+        if (args.length!=1) throw new UnsupportedOperationException("main to be called with filename");
+        String file=args[0];
+        
+        Little little=new Little(org.openprovenance.prov.xml.ProvFactory.getFactory());
+        little.openingBanner();
+        little.doConversions(little.makeLittle(), file);
+        little.closingBanner();
 
-
-
-
-
-
+    }
 
 }

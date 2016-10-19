17619bfddd71d454202f084bd0233d3300c372a2
Luc Moreau <luc.moreau@uprovenance.com>
2016-10-19 15:55:45 +0100

    refactoring overall workflow

--- tutorial/tutorial5/src/main/java/org/openprovenance/prov/tutorial/tutorial5/ProvenanceChallenge1Template.java
+++ tutorial/tutorial5/src/main/java/org/openprovenance/prov/tutorial/tutorial5/ProvenanceChallenge1Template.java
@@ -83,70 +83,7 @@ public class ProvenanceChallenge1Template  extends ChallengeCommon<Collection<Bi
         
     }
 
-    public class alignBindingBean {
-        private String imgfile1;
-        private String imglabel; 
-        private String hdrfile1;
-        private String hdrlabel;
-        private String imgreffile1;
-        private String imgreflabel; 
-        private String hdrreffile1;
-        private String hdrreflabel; 
-        private String activity; 
-        private String warpfile;
-        private String warplabel;
-        private String workflow;
-        private String agent;
-        public Collection<Bindings> export() {
-            return align(imgfile1, imglabel,
-                         hdrfile1, hdrlabel,
-                         imgreffile1, imgreflabel,
-                         hdrreffile1, hdrreflabel,
-                         activity,
-                         warpfile, warplabel,
-                         workflow, agent);
-        }
-        public void setImgFile(String imgfile1) {
-            this.imgfile1=imgfile1;
-        }
-        public void setImgLabel(String imglabel) {
-            this.imglabel=imglabel;
-        }
-        public void setHdrFile(String hdrfile1) {
-            this.hdrfile1=hdrfile1;
-        }
-        public void setHdrLabel(String hdrlabel) {
-            this.hdrlabel=hdrlabel;
-        }
-        public void setImgRefFile(String imgreffile1) {
-            this.imgreffile1=imgreffile1;
-        }
-        public void setImgRefLabel(String imgreflabel) {
-            this.imgreflabel=imgreflabel;
-        }
-        public void setHdrRefFile(String hdrreffile1) {
-            this.hdrreffile1=hdrreffile1;
-        }
-        public void setHdrRefLabel(String hdrreflabel) {
-            this.hdrreflabel=hdrreflabel;
-        }
-        public void setActivity(String activity) {
-            this.activity=activity;
-        }
-        public void setWarpfile(String warpfile) {
-            this.warpfile=warpfile;
-        }
-        public void setWarplabel(String warplabel) {
-            this.warplabel=warplabel;
-        }
-        public void setWorkflow(String workflow) {
-            this.workflow=workflow;
-        }
-        public void setAgent(String agent) {
-            this.agent=agent;
-        }
-    }
-
+  
     public Collection<Bindings> align(String imgfile1, String imglabel, 
                           String hdrfile1, String hdrlabel,
                           String imgreffile1, String imgreflabel, 

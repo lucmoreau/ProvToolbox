02ce31882cd078d17b8d8b187f10b77678550df3
Luc Moreau <l.moreau@ecs.soton.ac.uk>
2016-10-20 14:40:24 +0100

    tut6

---
+++ tutorial/tutorial6/src/main/java/org/openprovenance/prov/tutorial/tutorial5/ChallengeConstants.java
@@ -0,0 +1,43 @@
+package org.openprovenance.prov.tutorial.tutorial5;
+
+public interface ChallengeConstants {
+
+    public static final String PC1_NS = "http://www.ipaw.info/challenge/";
+    public static final String PC1_PREFIX = "pc1";
+    public static final String PRIM_NS = "http://openprovenance.org/primitives#";
+    public static final String PRIM_PREFIX = "prim";
+    
+    public static final String ALIGN_WARP = "Align_warp";
+    public static final String SOFTMEAN = "Softmean";
+    public static final String RESLICE = "Reslice";
+    public static final String SLICER = "Slicer";
+    public static final String CONVERT = "Convert";
+
+
+    public static final String STRING = "String";
+    public static final String FILE = "File";
+    
+    public static final String VALUE = "value";
+
+    public static final String ROLE_HDR_REF = "HdrRef";
+    public static final String ROLE_IMG_REF = "ImgRef";
+    public static final String ROLE_HDR = "Hdr";
+    public static final String ROLE_IMG = "Img";
+    public static final String ROLE_IN = "In";
+    public static final String ROLE_OUT = "Out";
+    public static final String ROLE_PARAM = "Param";
+    public static final String ROLE_H4 = "H4";
+    public static final String ROLE_I4 = "I4";
+    public static final String ROLE_H3 = "H3";
+    public static final String ROLE_I3 = "I3";
+    public static final String ROLE_H2 = "H2";
+    public static final String ROLE_I2 = "I2";
+    public static final String ROLE_H1 = "H1";
+    public static final String ROLE_I1 = "I1";
+    
+
+
+
+
+
+}
---
+++ tutorial/tutorial6/src/main/java/org/openprovenance/prov/tutorial/tutorial5/ProvenanceChallengeBeans.java
@@ -0,0 +1,77 @@
+package org.openprovenance.prov.tutorial.tutorial5;
+
+import java.util.Collection;
+
+import com.sun.xml.bind.v2.schemagen.episode.Bindings;
+
+public class ProvenanceChallengeBeans {
+
+
+    public class AlignBindingBean {
+        private String imgfile1;
+        private String imglabel; 
+        private String hdrfile1;
+        private String hdrlabel;
+        private String imgreffile1;
+        private String imgreflabel; 
+        private String hdrreffile1;
+        private String hdrreflabel; 
+        private String activity; 
+        private String warpfile;
+        private String warplabel;
+        private String workflow;
+        private String agent;
+
+
+        public Collection<Bindings> export(Challenge<Collection<Bindings>> proxy) {
+            return proxy.align(imgfile1, imglabel,
+                               hdrfile1, hdrlabel,
+                               imgreffile1, imgreflabel,
+                               hdrreffile1, hdrreflabel,
+                               activity,
+                               warpfile, warplabel,
+                               workflow, agent);
+        }
+        public void setImgFile(String imgfile1) {
+            this.imgfile1=imgfile1;
+        }
+        public void setImgLabel(String imglabel) {
+            this.imglabel=imglabel;
+        }
+        public void setHdrFile(String hdrfile1) {
+            this.hdrfile1=hdrfile1;
+        }
+        public void setHdrLabel(String hdrlabel) {
+            this.hdrlabel=hdrlabel;
+        }
+        public void setImgRefFile(String imgreffile1) {
+            this.imgreffile1=imgreffile1;
+        }
+        public void setImgRefLabel(String imgreflabel) {
+            this.imgreflabel=imgreflabel;
+        }
+        public void setHdrRefFile(String hdrreffile1) {
+            this.hdrreffile1=hdrreffile1;
+        }
+        public void setHdrRefLabel(String hdrreflabel) {
+            this.hdrreflabel=hdrreflabel;
+        }
+        public void setActivity(String activity) {
+            this.activity=activity;
+        }
+        public void setWarpfile(String warpfile) {
+            this.warpfile=warpfile;
+        }
+        public void setWarplabel(String warplabel) {
+            this.warplabel=warplabel;
+        }
+        public void setWorkflow(String workflow) {
+            this.workflow=workflow;
+        }
+        public void setAgent(String agent) {
+            this.agent=agent;
+        }
+    }
+
+
+}

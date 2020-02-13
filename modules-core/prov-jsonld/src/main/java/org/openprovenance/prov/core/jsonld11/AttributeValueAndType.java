ec474f2d71ad6d19065762d5d4e8995024e84199
Luc Moreau <luc.moreau@kcl.ac.uk>
2020-02-13 10:33:57 +0000

    refactoring jsonld deserialiser (attrib proxy data structure)

---
+++ modules-core/prov-jsonld/src/main/java/org/openprovenance/prov/core/jsonld11/AttributeValueAndType.java
@@ -0,0 +1,14 @@
+package org.openprovenance.prov.core.jsonld11;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.openprovenance.prov.core.jsonld11.serialization.Constants;
+
+
+public class AttributeValueAndType implements Constants {
+    @JsonProperty(PROPERTY_AT_TYPE)
+    public String typex;
+    @JsonProperty(PROPERTY_AT_VALUE)
+    public String value;
+    @JsonProperty(PROPERTY_STRING_LANG)
+    public String language;
+}
--- modules-core/prov-jsonld/src/main/java/org/openprovenance/prov/core/jsonld11/serialization/deserial/CustomAttributeDeserializer.java
+++ modules-core/prov-jsonld/src/main/java/org/openprovenance/prov/core/jsonld11/serialization/deserial/CustomAttributeDeserializer.java
@@ -1,9 +1,9 @@
 package org.openprovenance.prov.core.jsonld11.serialization.deserial;
 
 import com.fasterxml.jackson.core.JsonParser;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.DeserializationContext;
 import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+import org.openprovenance.prov.core.jsonld11.AttributeValueAndType;
 import org.openprovenance.prov.core.jsonld11.serialization.Constants;
 import org.openprovenance.prov.model.Attribute;
 import org.openprovenance.prov.model.Namespace;
@@ -15,7 +15,6 @@ import java.io.IOException;
 import java.util.Map;
 
 public class CustomAttributeDeserializer extends StdDeserializer<Attribute> implements Constants {
-
 
     static final ProvFactory pf=new ProvFactory();
 
@@ -29,13 +28,22 @@ public class CustomAttributeDeserializer extends StdDeserializer<Attribute> impl
         this(Attribute.class);
     }
 
-
     public CustomAttributeDeserializer(Class<?> vc) {
         super(vc);
     }
 
     @Override
-    public Attribute deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+    public Attribute deserialize(JsonParser jp, DeserializationContext deserializationContext) throws IOException {
+        if (jp.isExpectedStartObjectToken()) {
+            AttributeValueAndType valueAndType=jp.readValueAs(AttributeValueAndType.class);
+            QualifiedName elementName = (QualifiedName) deserializationContext.getAttribute(CustomKeyDeserializer.PROV_ATTRIBUTE_CONTEXT_KEY);
+            return deserialize_AttributeValueAndType(elementName,valueAndType,deserializationContext);
+        } else {
+            return deserialize(jp.getText(), deserializationContext);
+        }
+    }
+
+    public Attribute deserializeOLD(JsonParser jp, DeserializationContext deserializationContext) throws IOException {
         Object o=jp.readValueAs(Object.class);
 
         if (o instanceof String) {
@@ -51,7 +59,7 @@ public class CustomAttributeDeserializer extends StdDeserializer<Attribute> impl
     }
 
 
-    private final Attribute deserialize(String astring, DeserializationContext deserializationContext) throws IOException, JsonProcessingException {
+    private final Attribute deserialize(String astring, DeserializationContext deserializationContext) {
         Namespace ns= (Namespace) deserializationContext.getAttribute(CustomNamespaceDeserializer.CONTEXT_KEY_NAMESPACE);
         QualifiedName elementName = (QualifiedName) deserializationContext.getAttribute(CustomKeyDeserializer.PROV_ATTRIBUTE_CONTEXT_KEY);
         return pf.newAttribute(elementName, ns.stringToQualifiedName(astring,pf), PROV_QUALIFIED_NAME);
@@ -68,16 +76,56 @@ public class CustomAttributeDeserializer extends StdDeserializer<Attribute> impl
         QualifiedName typeQN;
         if (type == null || type.equals("xsd:string") || type.equals("prov:InternationalizedString")) {
             final String theLang = (String) map.get(Constants.PROPERTY_STRING_LANG);
-            valueObject = new LangString(textValue, (theLang == null) ? null : theLang);
-            typeQN = (theLang == null) ? XSD_STRING : PROV_INTERNATIONALIZED_STRING;
-        } else if (type.equals("prov:QUALIFIED_NAME")) {
-            final Namespace ns = (Namespace) deserializationContext.getAttribute(CustomNamespaceDeserializer.CONTEXT_KEY_NAMESPACE);
-            valueObject = ns.stringToQualifiedName(textValue, pf);
-            typeQN = ns.stringToQualifiedName(type, pf);
+            if (theLang==null) {
+                valueObject = new LangString(textValue, null);
+                typeQN =  XSD_STRING;
+            } else {
+                valueObject = new LangString(textValue, theLang);
+                typeQN = PROV_INTERNATIONALIZED_STRING;
+            }
         } else {
             final Namespace ns = (Namespace) deserializationContext.getAttribute(CustomNamespaceDeserializer.CONTEXT_KEY_NAMESPACE);
-            valueObject = textValue;
             typeQN = ns.stringToQualifiedName(type, pf);
+            if (type.equals("prov:QUALIFIED_NAME")) {
+                valueObject = ns.stringToQualifiedName(textValue, pf);
+            } else {
+                valueObject = textValue;
+            }
+        }
+
+
+        return pf.newAttribute(elementName, valueObject, typeQN);
+
+
+
+    }
+
+
+    private final Attribute deserialize_AttributeValueAndType(QualifiedName elementName, AttributeValueAndType valueAndType, DeserializationContext deserializationContext) {
+
+        final String type = valueAndType.typex;
+
+        final String textValue = valueAndType.value;
+
+        Object valueObject;
+        QualifiedName typeQN;
+        if (type == null || type.equals("xsd:string") || type.equals("prov:InternationalizedString")) {
+            final String theLang = valueAndType.language;
+            if (theLang==null) {
+                valueObject = new LangString(textValue, null);
+                typeQN =  XSD_STRING;
+            } else {
+                valueObject = new LangString(textValue, theLang);
+                typeQN = PROV_INTERNATIONALIZED_STRING;
+            }
+        } else {
+            final Namespace ns = (Namespace) deserializationContext.getAttribute(CustomNamespaceDeserializer.CONTEXT_KEY_NAMESPACE);
+            typeQN = ns.stringToQualifiedName(type, pf);
+            if (type.equals("prov:QUALIFIED_NAME")) {
+                valueObject = ns.stringToQualifiedName(textValue, pf);
+            } else {
+                valueObject = textValue;
+            }
         }
 
 

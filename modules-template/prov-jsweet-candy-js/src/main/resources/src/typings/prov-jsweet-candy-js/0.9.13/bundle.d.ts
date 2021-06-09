a86ed4b9ce235715c2bb129e3ac00b0b2f5aa31a
lucmoreau <luc.moreau@kcl.ac.uk>
2021-06-09 18:12:40 +0100

    post-release

---
+++ modules-template/prov-jsweet-candy-js/src/main/resources/src/typings/prov-jsweet-candy-js/0.9.13/bundle.d.ts
@@ -0,0 +1,23 @@
+/* Generated from Java with JSweet 3.0.0 - http://www.jsweet.org */
+/**
+ * Example: <code>sprintf(
+ * "The first 4 letters of the English alphabet are: %s, %s, %s and %s",
+ * "a", "b", "c", "d")</code>
+ * @param {string} format
+ * @param {java.lang.Object[]} args
+ * @return {string}
+ */
+declare function sprintf(format: string, ...args: any[]): string;
+
+/**
+ * Example: <code>vprintf(
+ * "The first 4 letters of the English alphabet are: %s, %s, %s and %s", new
+ * String[] {"a", "b", "c", "d"})</code>
+ * @param {string} format
+ * @param {java.lang.Object[]} args
+ * @return {string}
+ */
+declare function vprintf(format: string, args: any[]): string;
+
+
+

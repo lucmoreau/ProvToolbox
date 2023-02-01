c1547fb9e06ce8069c7016a0ff6e104399bd9334
Luc Moreau <luc.moreau@kcl.ac.uk>
2023-02-01 21:41:59 +0000

    changed insertion in array function

--- modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/sql/CompilerSqlComposer.java
+++ modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/sql/CompilerSqlComposer.java
@@ -218,32 +218,30 @@ public class CompilerSqlComposer {
                 .stream()
                 .filter(key -> isOutput.test(key) && shared.contains(key))
                 .collect(Collectors.toMap(  this::table_tokens,
-                                            key ->  (pp0) -> QueryBuilder.select(TOKEN_VAR_NAME, "id", insert_into_table(table.apply(key), additionalColumnsWithoutAlias2(templateBindingsSchema,key))).apply(pp0)
-                                                    .from(
-                                                                (pp) -> QueryBuilder.select(makeArray(TOKEN_VAR_NAME,
-                                                                                              nextVal(table.apply(key)),
-                                                                                              additionalColumnsWithAlias(templateBindingsSchema, key)))
-                                                                        .apply(pp)
-                                                                        .from((pp1) -> QueryBuilder.select(makeArray("DISTINCT ON (" + key + ") " + key + " AS " + TOKEN_VAR_NAME,
-                                                                                                                        additionalColumnsWithoutAlias(templateBindingsSchema,key)))
-                                                                                        .apply(pp1)
-                                                                                        .from(Constants.INPUT_TABLE),
-                                                            table_tokens0(key)),table_tokens(key)),
+                                            key ->  (pp0) ->
+                                                      QueryBuilder
+                                                              .select(TOKEN_VAR_NAME, "id", insert_into_table(table.apply(key), additionalColumnsWithoutAlias2(templateBindingsSchema,key)))
+                                                              .apply(pp0)
+                                                              .from(
+                                                                      (pp) ->
+                                                                        QueryBuilder
+                                                                                .select(makeArray(  TOKEN_VAR_NAME,
+                                                                                                    nextVal(table.apply(key)),
+                                                                                                    additionalColumnsWithAlias(templateBindingsSchema, key)))
+                                                                                .apply(pp)
+                                                                                .from((pp1) ->
+                                                                                        QueryBuilder
+                                                                                                .select(makeArray("DISTINCT ON (" + key + ") " + key + " AS " + TOKEN_VAR_NAME,
+                                                                                                        additionalColumnsWithoutAlias(templateBindingsSchema,key)))
+                                                                                                .apply(pp1)
+                                                                                                .from(Constants.INPUT_TABLE),
+                                                                                        table_tokens0(key)),
+                                                                      table_tokens(key)),
                                             (x, y) -> y,
                                             () -> new LinkedHashMap<String, Function<PrettyPrinter, ?>>() {{
                                                 put(Constants.INPUT_TABLE, pp -> QueryBuilder.select("*").apply(pp).from("unnest (" + Constants.RECORDS_VAR + ")"));
                                             }}));
 
-        Map<String,Function<PrettyPrinter, Object>> cteValues2=descriptorUtils
-                .fieldNames(templateBindingsSchema)
-                .stream()
-                .filter(key -> isOutput.test(key) && shared.contains(key))
-                .collect(Collectors.toMap(  this::table_ids,
-                                            key -> (pp) -> QueryBuilder.select(insert_into_table(table.apply(key), additionalColumnsWithoutAlias2(templateBindingsSchema,key))).apply(pp)
-                                                    .from(table_tokens(key)),
-                                            (x, y) -> y,
-                                            LinkedHashMap::new));
-       // cteValues1.putAll(cteValues2);
 
 
         List<String> insertColumns=descriptorUtils
@@ -282,60 +280,6 @@ public class CompilerSqlComposer {
                 "                $$ language SQL;\n" +
                 "\t\t\t\t"+
                 "\n\n";
-
-/**
-TODO: fix me:
-
-
-        CREATE OR REPLACE FUNCTION insert_setting_objective_composite_array(_records setting_objective_type [])
-        RETURNS table(ID INT,
-                objective INT,
-                policy1 INT,
-                setting INT)
-        AS $$
-        WITH
-        _input_table AS (SELECT *
-                FROM unnest (_records)),
-        _policy1_tokens AS (select _token, id, insert_into_policy(id, serial) as newid
-        from (SELECT _token,
-                cast(nextval('policy_id_seq') as INT) as id,
-                policy AS serial -- @sql.new.inputs
-        FROM (SELECT DISTINCT ON (policy1) policy1 AS _token, policy
-                FROM _input_table) _policy1_tokens0) _policy1_tokens0),
-        _setting_tokens AS (select _token, id, insert_into_activity(id) as newid
-        from (SELECT _token,
-                cast(nextval('activity_id_seq') as INT) as id
-                FROM (SELECT DISTINCT ON (setting) setting AS _token
-                        FROM _input_table) _setting_tokens0) _setting_tokens)
-
-
-        SELECT (_arecord).ID,
-                (_arecord).objective,
-                _policy1_id,
-                _setting_id
-        FROM (SELECT _policy1_token_id_pairs.id AS _policy1_id,
-                _setting_token_id_pairs.id AS _setting_id,
-                insert_setting_objective_composite(_policy1_token_id_pairs.id,
-                        policy0,
-                        policy,
-                        organization,
-                        management,
-                        _setting_token_id_pairs.id,
-                        time) AS _arecord
-                FROM _input_table
-                JOIN (SELECT _policy1_tokens.id,
-                        _policy1_tokens._token
-                        FROM _policy1_tokens) _policy1_token_id_pairs
-                ON policy1 = _policy1_token_id_pairs._token
-                JOIN (SELECT _setting_tokens.id,
-                        _setting_tokens._token
-                        FROM _setting_tokens) _setting_token_id_pairs
-                ON setting = _setting_token_id_pairs._token) inserted_rows
-        $$ language SQL;
-
-
- */
-
 
 
 

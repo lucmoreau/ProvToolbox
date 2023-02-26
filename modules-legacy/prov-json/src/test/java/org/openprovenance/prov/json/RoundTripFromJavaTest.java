1aafa7dfab4ff8f476df9fcc5269d59b1b4a42d0
Luc Moreau <luc.moreau@kcl.ac.uk>
2023-02-26 14:20:32 +0000

    upgraded version numbers

--- modules-legacy/prov-json/src/test/java/org/openprovenance/prov/json/RoundTripFromJavaTest.java
+++ modules-legacy/prov-json/src/test/java/org/openprovenance/prov/json/RoundTripFromJavaTest.java
@@ -11,10 +11,10 @@ import org.openprovenance.prov.xml.UncheckedTestException;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.github.fge.jackson.JsonLoader;
-import com.github.fge.jsonschema.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.exceptions.ProcessingException;
+import com.github.fge.jsonschema.core.report.ProcessingReport;
 import com.github.fge.jsonschema.main.JsonSchema;
 import com.github.fge.jsonschema.main.JsonSchemaFactory;
-import com.github.fge.jsonschema.report.ProcessingReport;
 
 /**
  * Round trip testing JavaBean -> PROV-JSON -> JavaBean

57219b813d17d89fc596fd665f29672d66e09f03
Luc Moreau <luc.moreau@kcl.ac.uk>
2023-08-25 17:20:36 +0100

    preparing for release

--- modules-core/prov-jsonld/src/main/java/org/openprovenance/prov/core/json/JSON_Document.java
+++ modules-core/prov-jsonld/src/main/java/org/openprovenance/prov/core/json/JSON_Document.java
@@ -13,8 +13,8 @@ public interface JSON_Document {
     @JsonFilter("nsFilter")
     @JsonProperty("context")
     Namespace getNamespace();
-
- 
+    @JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property = Constants.PROPERTY_BLOCK_TYPE)
+    @JsonSubTypes({
          @JsonSubTypes.Type(value = WasInfluencedBy.class,    name = Constants.PROPERTY_PROV_INFLUENCE),
             @JsonSubTypes.Type(value = WasInformedBy.class,      name = Constants.PROPERTY_PROV_COMMUNICATION),
             @JsonSubTypes.Type(value = WasDerivedFrom.class,     name = Constants.PROPERTY_PROV_DERIVATION),

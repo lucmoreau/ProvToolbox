00a12ce76af4558cda654f6e51020a1cd8288b1a
Luc Moreau <luc.moreau@kcl.ac.uk>
2023-09-15 08:15:41 +0100

    removed old copy of code

--- modules-template/prov-template-compiler/templates_cli_with_continuations/.jsweet/candies/typings/src/typings/prov-jsweet-candy-java/0.9.11-SNAPSHOT/bundle.d.ts
+++
@@ -1,5 +0,0 @@
-declare namespace org.openprovenance.apache.commons.lang {
-    class StringEscapeUtils {
-        static escapeJavaScript(format: string): string;
-    }
-}
--- modules-template/prov-template-compiler/templates_cli_with_continuations/src/main/java/org/openprovenance/prov/client/LoggerInterface.java
+++
@@ -1,6 +0,0 @@
-// Generated Automatically by ProvToolbox (org.openprovenance.prov.template.compiler.CompilerLogger) for templates config "templates"
-package org.openprovenance.prov.client;
-
-public interface LoggerInterface {
-  Builder[] getBuilders();
-}

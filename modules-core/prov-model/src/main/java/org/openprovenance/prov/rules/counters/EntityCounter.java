87a9f85f65e009b077c6b37e559e80e73100f383
Luc Moreau <luc.moreau@kcl.ac.uk>
2024-06-05 10:35:31 +0100

    prov-model: provenance statistics

---
+++ modules-core/prov-model/src/main/java/org/openprovenance/prov/rules/counters/EntityCounter.java
@@ -0,0 +1,7 @@
+package org.openprovenance.prov.rules.counters;
+
+public class EntityCounter  {
+    public int countEntities=0;
+    public int countGeneratedBy=0;
+    public int countDerivedFrom=0;
+}

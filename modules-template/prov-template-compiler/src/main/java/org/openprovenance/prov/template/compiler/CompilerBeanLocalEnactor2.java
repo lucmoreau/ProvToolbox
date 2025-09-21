8ea16ed4b022259c78717b6f55af17e33dab078f
Luc Moreau <luc.moreau@kcl.ac.uk>
2025-09-21 17:05:42 +0100

    prov-template-compiler: generation of local enactor (CompilerBeanLocalEnactor2)

--- modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/CompilerBeanLocalEnactor2.java
+++ modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/CompilerBeanLocalEnactor2.java
@@ -5,11 +5,14 @@ import org.openprovenance.prov.model.ProvFactory;
 import org.openprovenance.prov.template.compiler.common.BeanDirection;
 import org.openprovenance.prov.template.compiler.common.Constants;
 import org.openprovenance.prov.template.compiler.configuration.*;
+import org.openprovenance.prov.template.descriptors.Descriptor;
 import org.openprovenance.prov.template.descriptors.DescriptorUtils;
 import org.openprovenance.prov.template.descriptors.TemplateBindingsSchema;
 
 import javax.lang.model.element.Modifier;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import static org.openprovenance.prov.template.compiler.ConfigProcessor.*;
@@ -45,8 +48,15 @@ public class CompilerBeanLocalEnactor2 {
                 .addParameter(ParameterSpec.builder(String.class,"counter").build())
                 .returns(Integer.class);
 
+        MethodSpec.Builder method2 = MethodSpec.methodBuilder("newSIdentifier")
+                .addModifiers(Modifier.PUBLIC,Modifier.ABSTRACT)
+                .addParameter(ParameterSpec.builder(String.class,"field").build())
+                .addParameter(ParameterSpec.builder(String.class,"counter").build())
+                .returns(String.class);
+
 
         builder.addMethod(method1.build());
+        builder.addMethod(method2.build());
 
 
 
@@ -72,6 +82,7 @@ public class CompilerBeanLocalEnactor2 {
 
             if (config instanceof SimpleTemplateCompilerConfig) {
                 TemplateBindingsSchema bindingsSchema=compilerUtil.getBindingsSchema((SimpleTemplateCompilerConfig) config);
+                final Map<String, List<Descriptor>> theVar=bindingsSchema.getVar();
 
 
 
@@ -79,13 +90,22 @@ public class CompilerBeanLocalEnactor2 {
                 for (String field: descriptorUtils.fieldNames(bindingsSchema)) {
                     if (descriptorUtils.isOutput(field,bindingsSchema)) {
                         Optional<String> sqlTable=descriptorUtils.getSqlTable(field,bindingsSchema);
-                        mspec.addStatement("$N.$N = newIdentifier($S,$S)", OUT_BEAN, field, field, sqlTable.orElse("sql/" + field));
+
+                        Class<?> theType=compilerUtil.getJavaTypeForDeclaredType(theVar, field);
+                        if (theType==String.class)
+                            mspec.addStatement("$N.$N = newSIdentifier($S,$S)", OUT_BEAN, field, field, sqlTable.orElse("sql/" + field));
+                        else
+                            mspec.addStatement("$N.$N = newIdentifier($S,$S)", OUT_BEAN, field, field, sqlTable.orElse("sql/" + field));
                     }
                 }
+                mspec.addStatement("$N.$N = newIdentifier($S,$S)", OUT_BEAN, "ID", "template/"+ config.name, "template/"+config.name);
 
 
-            mspec.addStatement("return $N", OUT_BEAN);
+                mspec.addStatement("return $N", OUT_BEAN);
 
+            } else {
+                mspec.addComment("Local enactor for complex templates not implemented yet");
+                mspec.addStatement("return $N", OUT_BEAN);
             }
 
             builder.addMethod(mspec.build());

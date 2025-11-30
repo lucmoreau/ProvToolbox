77cb944468ceb522a006c650a6e9c2299ec262d9
Luc Moreau <luc.moreau@kcl.ac.uk>
2025-11-30 11:33:38 +0000

    prov-template-compiler: removed dependency on java 12, but introduced MAVEN_OPTS to support jsweet

--- modules-template/prov-jsweet-candy-java/src/main/jsweet/jsweet.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn -f ${project.build.directory}/jsweet/pom-jsweet.xml jsweet:jsweet
--- modules-template/prov-template-library/src/main/jsweet/jsweet-with-maven-opts.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Setting MAVEN_OPTS for JSweet compilation"
-export MAVEN_OPTS="--add-opens jdk.compiler/com.sun.tools.javac.api=ALL-UNNAMED  --add-opens jdk.compiler/com.sun.tools.javac.code=ALL-UNNAMED  --add-opens jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED  --add-opens jdk.compiler/com.sun.tools.javac.model=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.parser=ALL-UNNAMED --add-opens jdk.compiler/com.sun.tools.javac.tree=ALL-UNNAMED  --add-opens jdk.compiler/com.sun.tools.javac.util=ALL-UNNAMED"
-mvn -f ${project.build.directory}/jsweet/pom-jsweet.xml -Dlibrary.source=${project.build.directory}/generated-sources/${package.name.cli} jsweet:jsweet

3c734e26dccbb0224dde28ae1313fff6df50f4a8
Luc Moreau <luc.moreau@sussex.ac.uk>
2025-12-04 19:20:19 +0000

    prov-toolbox-plugin: baseDir for Validate

--- modules-tools/prov-toolbox-plugin/src/main/java/org/openprovenance/plugin/ttf/BatchExecutor.java
+++ modules-tools/prov-toolbox-plugin/src/main/java/org/openprovenance/plugin/ttf/BatchExecutor.java
@@ -7,17 +7,13 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
-import org.openprovenance.prov.interop.DeserializerFunction;
 import org.openprovenance.prov.interop.InteropFramework;
-import org.openprovenance.prov.interop.SerializerFunction;
 import org.openprovenance.prov.model.ProvDeserialiser;
 import org.openprovenance.prov.model.ProvSerialiser;
-import org.openprovenance.prov.model.interop.Formats;
 
-import java.lang.reflect.Method;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLClassLoader;
+import java.nio.file.Path;
 import java.util.*;
 
 @Mojo(name = "execute-ttf", defaultPhase = LifecyclePhase.PROCESS_RESOURCES)
@@ -26,57 +22,22 @@ public class BatchExecutor extends AbstractMojo {
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     private MavenProject project;
 
-    @Parameter(property = "execute-ttf.baseDir")
-    private String baseDir;
+    @Parameter(property = "validate-ttf.debug", defaultValue = "false")
+    private boolean debug;
+
+    @Parameter(property = "execute-ttf.warning", defaultValue = "false")
+    private boolean warning;
+
     @Parameter(property = "execute-ttf.inputBaseDir")
     private String inputBaseDir;
+
     @Parameter(property = "execute-ttf.outputBaseDir")
     private String outputBaseDir;
 
 
-    @Parameter(property = "execute-ttf.configs", required = true)
-    private List<String> configs = new ArrayList<>();
+    @Parameter(property = "execute-ttf.args", required = true)
+    private List<String> args = new ArrayList<>();
 
-    public void executeOld() throws MojoExecutionException {
-        String className= org.openprovenance.prov.template.expander.ttf.BatchExecutor.class.getName();
-
-        try {
-            if ((baseDir!=null) && (inputBaseDir!=null)) {
-                throw new MojoExecutionException("Both baseDir and inputBaseDir are defined");
-            }
-            if ((baseDir!=null) && (outputBaseDir!=null)) {
-                throw new MojoExecutionException("Both baseDir and outputBaseDir are defined");
-            }
-
-            List<String> classpathElements = project.getCompileClasspathElements();
-            URL[] urls = new URL[classpathElements.size()];
-            for (int i = 0; i < classpathElements.size(); i++) {
-                urls[i] = new URL("file://" + classpathElements.get(i));
-            }
-           // System.out.println("urls=" + urls);
-           // System.out.println("className=" + className);
-            List<String> finalList=new LinkedList<>();
-            if (baseDir!=null) {
-                finalList.add(baseDir);
-            } else {
-                if (outputBaseDir==null) outputBaseDir="";
-                if (inputBaseDir==null) inputBaseDir="";
-                finalList.add(inputBaseDir);
-                finalList.add(outputBaseDir);
-            }
-            finalList.add("-configs");
-            finalList.addAll(configs);
-
-            //System.out.println("configs:=" + finalList);
-            URLClassLoader loader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
-            Class<?> clazz = Class.forName(className, true, loader);
-            Method method = clazz.getMethod("main", String[].class);
-            String[] argsArray = finalList.toArray(new String[0]);
-            method.invoke(null, (Object) argsArray);  // cast is necessary to avoid varargs issues
-        } catch (Exception e) {
-            throw new MojoExecutionException("Failed to execute class " + className, e);
-        }
-    }
     public void execute() throws MojoExecutionException {
         InteropFramework interop= new InteropFramework();
 
@@ -86,38 +47,41 @@ public class BatchExecutor extends AbstractMojo {
 
         interop.populateSerializerDeserializerMaps(deserializerMap2, serializerMap2);
 
+        if (debug) {
+            getLog().info(getClass().getName());
+        }
+
 
         // String className= org.openprovenance.prov.template.expander.ttf.BatchExecutor.class.getName();
 
         try {
-            if ((baseDir!=null) && (inputBaseDir!=null)) {
-                throw new MojoExecutionException("Both baseDir and inputBaseDir are defined");
+            if ((inputBaseDir==null) || (inputBaseDir.isEmpty())) {
+                throw new MojoExecutionException("BatchExecutor: inputBaseDir not defined");
             }
-            if ((baseDir!=null) && (outputBaseDir!=null)) {
-                throw new MojoExecutionException("Both baseDir and outputBaseDir are defined");
+            if ((outputBaseDir==null) || (outputBaseDir.isEmpty())) {
+                throw new MojoExecutionException("BatchExecutor: outputBaseDir not defined");
             }
 
-            List<String> classpathElements = project.getCompileClasspathElements();
-            URL[] urls = new URL[classpathElements.size()];
-            for (int i = 0; i < classpathElements.size(); i++) {
-                urls[i] = new URL("file://" + classpathElements.get(i));
+            if (debug  && (args==null || args.isEmpty())) {
+                getLog().info("BatchExecutor args is empty");
             }
-            //System.out.println("urls=" + urls);
-            //System.out.println("className=" + className);
+
             List<String> finalList=new LinkedList<>();
-            if (baseDir!=null) {
-                finalList.add(baseDir);
-            } else {
-                if (outputBaseDir==null) outputBaseDir="";
-                if (inputBaseDir==null) inputBaseDir="";
-                finalList.add(inputBaseDir);
-                finalList.add(outputBaseDir);
-            }
-            finalList.add("-configs");
-            finalList.addAll(configs);
 
-            //System.out.println("configs:=" + finalList);
-            //URLClassLoader loader = new URLClassLoader(urls, Thread.currentThread().getContextClassLoader());
+            finalList.add(inputBaseDir);
+            finalList.add(outputBaseDir);
+
+            finalList.add("-configs");
+            Path inputBasePath=java.nio.file.Paths.get(inputBaseDir);
+            for (String path: args) {
+                finalList.add(inputBasePath.resolve(path).toString());
+            }
+
+            if (debug) {
+                getLog().info("BatchExecutor args: " + finalList);
+            }
+
+
             org.openprovenance.prov.template.expander.ttf.BatchExecutor executor=new org.openprovenance.prov.template.expander.ttf.BatchExecutor( serializerMap2, deserializerMap2);
             //Class<?> clazz = Class.forName(className, true, loader);
             //Method method = clazz.getMethod("main", String[].class);
@@ -125,7 +89,7 @@ public class BatchExecutor extends AbstractMojo {
             //method.invoke(null, (Object) argsArray);  // cast is necessary to avoid varargs issues
             executor.execute(argsArray);
 
-        } catch (RuntimeException | DependencyResolutionRequiredException | MalformedURLException e) {
+        } catch (RuntimeException e) {
             e.printStackTrace();
             throw new MojoExecutionException("Failed to execute execute-ttf " , e);
         }
--- modules-tools/prov-toolbox-plugin/src/main/java/org/openprovenance/plugin/ttf/TtbSchemaValidator.java
+++ modules-tools/prov-toolbox-plugin/src/main/java/org/openprovenance/plugin/ttf/TtbSchemaValidator.java
@@ -23,22 +23,33 @@ public class TtbSchemaValidator extends AbstractMojo {
     @Parameter(defaultValue = "${project}", readonly = true, required = true)
     private MavenProject project;
 
+    @Parameter(property = "validate-ttf.debug", defaultValue = "false")
+    private boolean debug;
+
     @Parameter(property = "validate-ttf.warning", defaultValue = "false")
     private boolean warning;
 
+    @Parameter(property = "validate-ttf.inputBaseDir")
+    private String inputBaseDir;
 
     @Parameter(property = "validate-ttf.args")
     private List<String> args = new ArrayList<>();
 
     public void execute() throws MojoExecutionException {
+
+        if (debug) {
+            getLog().info(getClass().getName());
+        }
+
+        if ((inputBaseDir==null) || (inputBaseDir.isEmpty())) {
+            throw new MojoExecutionException("Validate TTF: inputBaseDir is not defined " + args);
+        }
         try {
             List<String> classpathElements = project.getCompileClasspathElements();
             URL[] urls = new URL[classpathElements.size()];
             for (int i = 0; i < classpathElements.size(); i++) {
                 urls[i] = new URL("file://" + classpathElements.get(i));
             }
-            //System.out.println("urls=" + urls);
-            //System.out.println("args=" + args);
 
             // copy file f to a temporary file with name openprovenance-ttf-schema-${project.version}.json
             String projectVersion = project.getVersion(); // get the project version
@@ -48,19 +59,31 @@ public class TtbSchemaValidator extends AbstractMojo {
             Path tempFilePath = Paths.get(System.getProperty("java.io.tmpdir"), tempFileName);
 
             IOUtils.copy(Expand.class.getClassLoader().getResourceAsStream("jsonschema/template-transformation-file-schema.json"), Files.newOutputStream(tempFilePath));
+            Path inputBasePath=java.nio.file.Paths.get(inputBaseDir);
 
             for (String arg : args) {
+                Path absolutePath=inputBasePath.resolve(arg);
+                // check file exists
+                if (!Files.exists(absolutePath)) {
+                    if (warning) {
+                        getLog().warn("File does not exist: " + absolutePath);
+                        continue;
+                    } else {
+                        throw new MojoExecutionException("File does not exist: " + absolutePath);
+                    }
+                }
+
 
                 // call command line executable ajv with argument f.getAbsolutePath()
-                ProcessBuilder pb = new ProcessBuilder("ajv", "validate", "-s", tempFilePath.toString(), "-d", arg);
+                ProcessBuilder pb = new ProcessBuilder("ajv", "validate", "-s", tempFilePath.toString(), "-d", absolutePath.toString());
                 pb.inheritIO();
                 Process p = pb.start();
                 int exitCode = p.waitFor();
                 if (exitCode != 0) {
                     if (warning) {
-                        getLog().warn("Failed to validate " + arg);
+                        getLog().warn("Failed to validate " + absolutePath);
                     } else {
-                        throw new MojoExecutionException("Failed to validate " + arg);
+                        throw new MojoExecutionException("Failed to validate " + absolutePath);
                     }
                 }
 

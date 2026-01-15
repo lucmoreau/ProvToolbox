9f2be89577b61a6487c4ea1cf0a0b3e78967c1ae
Luc Moreau <luc.moreau@sussex.ac.uk>
2026-01-15 21:42:07 +0000

    prov-template-compiler: introduced PAST (provenance abstract syntax tree), intermediate to Poet and Python (WIP)

---
+++ modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/past/annotations/RegisterMethod.java
@@ -0,0 +1,9 @@
+package org.openprovenance.prov.template.compiler.past.annotations;
+
+public class RegisterMethod extends PythonAnnotation {
+    public static final String NAME = "python:@registermethod";
+
+    public RegisterMethod() {
+        super(NAME);
+    }
+}

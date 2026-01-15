e1eccaf1db715171f5fa7a2740330bbfd0f6a459
Luc Moreau <luc.moreau@sussex.ac.uk>
2026-01-15 21:42:20 +0000

    prov-template-compiler: introduced PAST (provenance abstract syntax tree), intermediate to Poet and Python (WIP)

---
+++ modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/past/annotations/SingleDispatchMethod.java
@@ -0,0 +1,9 @@
+package org.openprovenance.prov.template.compiler.past.annotations;
+
+public class SingleDispatchMethod extends PythonAnnotation {
+    public static final String NAME = "python:@singledispatchmethod";
+
+    public SingleDispatchMethod() {
+        super(NAME);
+    }
+}

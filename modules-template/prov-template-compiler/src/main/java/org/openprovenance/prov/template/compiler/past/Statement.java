999ba2a96f2614a7ed00b2c29337ab5efbffc52d
Luc Moreau <luc.moreau@sussex.ac.uk>
2026-02-10 20:09:18 +0000

    prov-template-compiler: introduced PAST (provenance abstract syntax tree), intermediate to Poet and Python (WIP)

--- modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/past/Statement.java
+++ modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/past/Statement.java
@@ -2,7 +2,8 @@ package org.openprovenance.prov.template.compiler.past;
 
 
 public abstract class Statement {
-    public enum StatementKind {RETURN, ASSIGNMENT, DECLARATION, COMMENT, IF_STATEMENT, FOR_LOOP, DO_LOOP, ITERATOR, EXPRESSION_STATEMENT, DEFINITION}
+    public enum StatementKind {RETURN, ASSIGNMENT, DECLARATION, COMMENT, IF_STATEMENT, FOR_LOOP, DO_LOOP,
+        ITERATOR, EXPRESSION_STATEMENT, DEFINITION, TRY_CATCH, THROW, SUPER_CONSTRUCTOR_CALL}
     public StatementKind statementKind;
 
 }

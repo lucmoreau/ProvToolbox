30c4f02c8fd17f5b74c46a93332e3b9284d324fc
Luc Moreau <luc.moreau@sussex.ac.uk>
2026-03-16 08:30:50 +0000

    prep release

--- modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/past/annotations/StatefulProcessor.java
+++ modules-template/prov-template-compiler/src/main/java/org/openprovenance/prov/template/compiler/past/annotations/StatefulProcessor.java
@@ -1,9 +1,9 @@
 package org.openprovenance.prov.template.compiler.past.annotations;
 
 /**
- * Rust annotation to mark a method as requiring mutable self (&mut self).
+ * Rust annotation to mark a method as requiring mutable self (&amp;mut self).
  *
- * By default, Rust methods are generated with immutable self (&self) for stateless processors.
+ * By default, Rust methods are generated with immutable self (&amp;self) for stateless processors.
  * Use this annotation when the method needs to mutate internal state.
  *
  * Usage in PAST code:
@@ -13,7 +13,7 @@ package org.openprovenance.prov.template.compiler.past.annotations;
  *
  * This will generate:
  * <pre>
- * fn process(&mut self, ...) -> T  // instead of &self
+ * fn process(&amp;mut self, ...) -> T  // instead of &amp;self
  * </pre>
  */
 public class StatefulProcessor extends RustAnnotation {
